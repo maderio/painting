56082512afcf951f5b023245b02ded2c39a0ca3b
Rio Renata <33323600+maderio@users.noreply.github.com>
2018-08-26 20:05:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -17,5 +17,6 @@ def do_get():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0', port=port)
